2883bba906824d1937a684d78682edcb9f157d0d
DraganovTs <cvetomir_draganov@abv.bg>
2024-09-07 19:22:01 +0300

    initial commit

---
+++ src/test/java/homecode/opteamer/OpteamerApplicationTests.java
@@ -0,0 +1,13 @@
+package homecode.opteamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpteamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
