e26871eb61c444919574f3d41eb8447ccc9ad0d9
DraganovTs <cvetomir_draganov@abv.bg>
2024-09-17 19:56:08 +0300

    css

--- opteamer-client/src/app/login/login.component.ts
+++ opteamer-client/src/app/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: '../app.component.css'
 })
 export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(private router: Router){
+  }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup<any>({
+      'email': new FormControl(null,[Validators.required,Validators.email]),
+      'pwd': new FormControl( null, [Validators.required]),
+    });
+  }
+
+  onSubmit(){
+
+  }
 
 }
