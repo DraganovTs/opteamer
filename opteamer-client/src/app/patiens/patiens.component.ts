360def37897cc5e9cab1aa38d651a5416678942f
DraganovTs <cvetomir_draganov@abv.bg>
2024-09-19 21:08:58 +0300

    Patients module

---
+++ opteamer-client/src/app/patiens/patiens.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patiens',
+  templateUrl: './patiens.component.html',
+  styleUrl: './patiens.component.css'
+})
+export class PatiensComponent {
+
+}
