11c3bed717dee8d4d8f78e58a4da135b50ca7b8f
DraganovTs <cvetomir_draganov@abv.bg>
2024-09-21 12:09:26 +0300

    teamMember service and CRUD operations to back end

---
+++ opteamer-client/src/app/teammember/teammember.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teammember',
+  templateUrl: './teammember.component.html',
+  styleUrl: './teammember.component.css'
+})
+export class TeammemberComponent {
+
+}
