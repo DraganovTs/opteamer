abf9fbefb2c3c3f454ce6da76c0f41940f2f9bec
DraganovTs <cvetomir_draganov@abv.bg>
2024-09-22 07:57:12 +0300

    Operation type component

---
+++ opteamer-client/src/app/operationtype/operationtype.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operationtype',
+  templateUrl: './operationtype.component.html',
+  styleUrl: './operationtype.component.css'
+})
+export class OperationtypeComponent {
+
+}
