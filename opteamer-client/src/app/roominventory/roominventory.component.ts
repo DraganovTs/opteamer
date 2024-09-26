966b47b4c4990d71932f0b8f1ce576934b8e3798
DraganovTs <cvetomir_draganov@abv.bg>
2024-09-26 21:45:01 +0300

    create room inventory component

---
+++ opteamer-client/src/app/roominventory/roominventory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roominventory',
+  templateUrl: './roominventory.component.html',
+  styleUrl: './roominventory.component.css'
+})
+export class RoominventoryComponent {
+
+}
