a871547f43bc53a4162e4450e29098fb2ae54326
DraganovTs <cvetomir_draganov@abv.bg>
2024-09-29 11:41:55 +0300

    Operation Room component completed

---
+++ opteamer-client/src/app/operationroom/operationroom.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operationroom',
+  templateUrl: './operationroom.component.html',
+  styleUrl: './operationroom.component.css'
+})
+export class OperationroomComponent {
+
+}
