8b10c8348be86681ce6779f03dfe38348d91add9
DraganovTs <cvetomir_draganov@abv.bg>
2024-10-02 20:59:13 +0300

    operation report component service

---
+++ opteamer-client/src/app/operationreport/operationreport.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operationreport',
+  templateUrl: './operationreport.component.html',
+  styleUrl: './operationreport.component.css'
+})
+export class OperationreportComponent {
+
+}
