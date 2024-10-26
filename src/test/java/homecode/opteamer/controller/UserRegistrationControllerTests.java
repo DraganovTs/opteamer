1a7f174ab94625a5c8ddde47001363f8187aca4d
DraganovTs <cvetomir_draganov@abv.bg>
2024-10-26 22:04:42 +0300

    User Registration tests

---
+++ src/test/java/homecode/opteamer/controller/UserRegistrationControllerTests.java
@@ -0,0 +1,12 @@
+package homecode.opteamer.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class UserRegistrationControllerTests {
+}
